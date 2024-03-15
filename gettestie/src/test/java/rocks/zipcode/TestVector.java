ad10f2b71fff9dc5dd6d0a83bf9bf77bf1cc2cbc
ShailyGH <shailykunal@.com>
2024-03-15 16:18:35 -0400

    TestVector completed.

--- gettestie/src/test/java/rocks/zipcode/TestVector.java
+++ gettestie/src/test/java/rocks/zipcode/TestVector.java
@@ -1,4 +1,38 @@
 package test.java.rocks.zipcode;
 
-public class TestVector {
+import org.junit.Test;
+
+import java.util.Vector;
+
+public class TestVector
+{
+    @Test
+    public void testIsEmpty()
+    {
+//       int n = 5;
+        Vector<Integer> vec1 = new Vector<Integer>();
+        System.out.println("Vector: " + vec1);
+        System.out.println("Is the Vector empty? " + vec1.isEmpty());
+    }
+
+    @Test
+    public void testremoveAll()
+    {
+        // Creating an empty Vector
+        Vector<Integer> vec1 = new Vector<Integer>();
+
+        // Use add() method to add elements in the Vector
+        vec1.add(4);
+        vec1.add(6);
+        vec1.add(8);
+        vec1.add(12);
+        vec1.add(24);
+
+        // Output the Vector
+        System.out.println("Vector: " + vec1);
+
+        // Creating an empty Vector
+
+        // Use add() method to add elements in the Vector
+    }
 }
