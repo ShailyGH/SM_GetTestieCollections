97b2032c0c277c6caf31f0ae99f958296fdb5ca8
ShailyGH <shailykunal@.com>
2024-03-15 21:59:15 -0400

    TestTreeMap  completed.

--- gettestie/src/test/java/rocks/zipcode/TestTreeMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestTreeMap.java
@@ -1,4 +1,18 @@
 package test.java.rocks.zipcode;
 
-public class TestTreeMap {
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestTreeMap
+{
+    @Test
+    public void testDescTail() {
+        var tree = new TreeMap<Integer, Integer>();
+        for (int i = 0; i < 10; ++i)
+            tree.put(i, i);
+        System.out.println(tree.tailMap(9));
+        System.out.println(tree.descendingMap().tailMap(4));
+    }
 }
