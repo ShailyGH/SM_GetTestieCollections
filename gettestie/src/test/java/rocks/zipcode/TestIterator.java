1a6dab2f2dbdbe01062ba9fb7dce64335d335aaa
ShailyGH <shailykunal@.com>
2024-03-15 21:50:20 -0400

    TestIterator  completed.

--- gettestie/src/test/java/rocks/zipcode/TestIterator.java
+++ gettestie/src/test/java/rocks/zipcode/TestIterator.java
@@ -3,22 +3,33 @@ package test.java.rocks.zipcode;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 
 public class TestIterator
 {
-//    @Test
-//    public void TestIterator1() {
-//
-//        LinkedList<String> list = new LinkedList<String>();
-//        list.add("Hi Jim");
-//        list.add("Fruit");
-//        list.add("Veggies");
-//        list.add("Eggs");
-//        Iterator iterator = list.iterator();
-//        int count = 1;
-//        while (iterator.hasNext()) {count++;}
-//        Assert.assertEquals(count, list.size());
+    @Test
+    public void TestIterator1() {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Hi Jim");
+        list.add("Fruit");
+        list.add("Veggies");
+        list.add("Eggs");
+        Iterator <String> it = list.iterator();
+        System.out.println(it.next());
+    }
+//    ArrayList<Integer> numbers = new ArrayList<Integer>();
+//    numbers.add(12);
+//    numbers.add(4);
+//    numbers.add(20);
+//    numbers.add(23);
+//    Iterator<Integer> it = numbers.iterator();
+//    while(it.hasNext()) {
+//    Integer i = it.next();
+//    if(i < 10) {
+//        it.remove();
 //    }
+//}
+//    System.out.println(numbers);
 }
