d8599a143ba6e085ea3f002056ba7dcbef71a23b
ShailyGH <shailykunal@.com>
2024-03-17 15:12:33 -0400

    Finished.

--- gettestie/src/test/java/rocks/zipcode/TestComparable.java
+++ gettestie/src/test/java/rocks/zipcode/TestComparable.java
@@ -1,9 +1,22 @@
 package test.java.rocks.zipcode;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class TestComparable
-{
-   // @Test
+import java.util.ArrayList;
+
+
+public class TestComparable {
+
+    @Test
+  public void testComparable(){
+      ArrayList<String> arrayList = new ArrayList<>();
+      arrayList.add("a");
+      arrayList.add("b");
+      arrayList.add("c");
+      Integer expected = -1;
+      Integer actual = arrayList.get(0).compareTo(arrayList.get(1));
+      Assert.assertEquals(expected, actual);
+  }
 
 }
