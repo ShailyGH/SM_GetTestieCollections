d90ddffb724fe3b6aeeadea5127ffe6191f133d5
Shaily Modi <shailykunal@gmail.com>
2024-03-23 13:53:37 -0400

    TestComparable completed

--- gettestie/src/test/java/rocks/zipcode/TestComparable.java
+++ gettestie/src/test/java/rocks/zipcode/TestComparable.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class TestComparable {
@@ -18,5 +20,35 @@ public class TestComparable {
       Integer actual = arrayList.get(0).compareTo(arrayList.get(1));
       Assert.assertEquals(expected, actual);
   }
+    @Test
+    public void testArraySort() {
 
+        int[] myActualArray = new int[] {9, 6, 10, 7};
+        int[] mySortedArray = new int[] {6, 7, 9, 10};
+
+        // Sorting the array
+        Arrays.sort(myActualArray);
+
+        Assert.assertArrayEquals(mySortedArray, myActualArray);
+    }
+
+    @Test
+    public void testCompareTo() {
+        List<String> expectedMarvelHeroes = new ArrayList<>();
+        List<String> actualMarvelHeroes = new ArrayList<>();
+        ArrayList<String> myArray = new ArrayList<>();
+        myArray.add("SpiderMan ");
+        myArray.add("Wolverine ");
+        myArray.add("Xavier ");
+        myArray.add("Cyclops ");
+
+        actualMarvelHeroes.addAll(myArray);
+
+        expectedMarvelHeroes.add("SpiderMan ");
+        expectedMarvelHeroes.add("Wolverine ");
+        expectedMarvelHeroes.add("Xavier ");
+        expectedMarvelHeroes.add("Cyclops ");
+
+        Assert.assertTrue(expectedMarvelHeroes.equals(actualMarvelHeroes));
+    }
 }
